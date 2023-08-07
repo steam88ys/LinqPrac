4676e82f489b6b77c668dd4b2e835a81e55ad894
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-07 14:00:01 +0900

    Linq를 사용하지 않은 세련되지 않은 방법

--- LinqPrac/Program.cs
+++ LinqPrac/Program.cs
@@ -10,6 +10,18 @@ namespace LinqPrac
     {
         static void Main(string[] args)
         {
+            // Linq를 사용하지 않은 세련되지 않은 정렬 방법
+            List<int> input = new List<int>() { 1, 2, 3, 4 };
+            List<int> output = new List<int>();
+
+            foreach (var item in input)
+            {
+                if (item % 2 == 0) output.Add(item);
+            }
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
